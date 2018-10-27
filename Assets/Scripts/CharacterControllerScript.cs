de00cf3bd048f353faca9aaf55da086041a82bed
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-27 17:02:07 -0500

    Add animation based on movement

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public bool facingRight = true;
+    private Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        myRigidBody.velocity = new Vector2(move * maxSpeed, myRigidBody.velocity.y);
+	}
+}
