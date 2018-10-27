60c51df01546209e18bec8ec0537a2eda1427d0b
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-27 17:11:52 -0500

    Add animator and movement speed to script

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
+    [SerializeField]
+    public float maxSpeed = 10f;
 
-    public float maxSpeed;
-    public bool facingRight = true;
+    private bool facingRight = true;
     private Rigidbody2D myRigidBody;
+
+    Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +24,26 @@ public class CharacterControllerScript : MonoBehaviour
         float move = Input.GetAxis("Horizontal");
         myRigidBody = GetComponent<Rigidbody2D>();
 
+        anim.SetFloat("speed", Mathf.Abs(move));
+
         myRigidBody.velocity = new Vector2(move * maxSpeed, myRigidBody.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
 	}
+
+    //Flips the character for left and right animation.
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
