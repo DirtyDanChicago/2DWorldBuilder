0d8f641ca488086ad6719c56afe956f5f1bf8b69
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-27 18:33:23 -0500

    Add ground check variables

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class CharacterControllerScript : MonoBehaviour
 {
     [SerializeField]
-    public float maxSpeed = 10f;
+    private float maxSpeed = 10f;
+    [SerializeField]
+    private float jumpForce = 700f;
+
+    public bool grounded = false;
+    public float groundRadius = 0.2f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
 
     private bool facingRight = true;
     private Rigidbody2D myRigidBody;
 
+
     Animator anim;
 
 	// Use this for initialization
-	void Start ()
+	private void Start ()
     {
         anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	private void FixedUpdate ()
     {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("ground", grounded);
+
+        anim.SetFloat("vspeed", myRigidBody.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
         myRigidBody = GetComponent<Rigidbody2D>();
 
@@ -38,8 +52,17 @@ public class CharacterControllerScript : MonoBehaviour
         }
 	}
 
+    private void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("ground", false);
+            myRigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
     //Flips the character for left and right animation.
-    void Flip()
+    private void Flip()
     {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
